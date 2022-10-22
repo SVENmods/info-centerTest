3366eaa8f92e70bfba69b4d8ecffb7cc96687473
SVENmods <94043508+SVENmods@users.noreply.github.com>
2022-10-23 00:38:58 +0300

    made full page for desk/laptop

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.querySelector("#range-inp").addEventListener("change", function (e) {
+  document.querySelector("#range-percent").innerHTML = e.target.value + "%";
+});
+
+function uploadFile(target) {
+  document.getElementById("file-name").innerHTML = target.files[0].name;
+}
+
+const linkVal = document.querySelectorAll(".dropdown-content-value");
+var link = new Array(linkVal.length);
+for (var i = linkVal.length; i--; ) {
+  link[i] = linkVal[i].addEventListener("click", function (e) {
+    document.querySelector("#input-select-selected-value").innerHTML =
+      e.target.innerHTML;
+  });
+}
